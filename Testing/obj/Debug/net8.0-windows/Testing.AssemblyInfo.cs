612b27120e1a21f197c38f40cac506f04c81f15e
Pratyush Kakkar <pratyushk1010@gmail.com>
2025-04-24 21:54:19 +0100

    Update ReadMe

--- Testing/obj/Debug/net8.0-windows/Testing.AssemblyInfo.cs
+++ Testing/obj/Debug/net8.0-windows/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558d4abe303cf6d9ae125d4c673c321ba72509de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00334d215949b4de9138d7d7f7c55f17cba6dfeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
